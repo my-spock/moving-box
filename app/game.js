37dd0d6295565d66ce07afab8b38c8f33c427dcb
my-spock <rplowman15@gmail.com>
2016-05-06 21:08:47 -0600

    push

--- app/game.js
+++ app/game.js
@@ -1,19 +1,45 @@
 app.controller('GameController', function() {
     var gc = this;
     
-    gc.vertical = 0;
-    gc.horizontal = 0;
-    
-    gc.movePlayer = function(e) {
-        if (e.keyCode == 40) {  //down arrow
-            gc.vertical++;
-        } else if (e.keyCode == 38) {  //up arrow
-            gc.vertical--;
-        } else if (e.keyCode == 37) {  //left arrow
-            gc.horizontal--;
-        } else if (e.keyCode == 39) {  //right arrow
-            gc.horizontal++;
+    gc.bluebox = {
+        mods: {
+            moveSpeed: 2
+        },
+        position: {
+            top: 0,
+            left: 0
         }
     }
+    
+    gc.movePlayer = function(e) {
+        var moveSpeed = 1 * gc.bluebox.mods.moveSpeed;
+        
+        if (e.keyCode == 40) {  //down arrow
+            gc.bluebox.position.top += moveSpeed;
+        } else if (e.keyCode == 38) {  //up arrow
+            gc.bluebox.position.top -= moveSpeed;
+        } else if (e.keyCode == 37) {  //left arrow
+            gc.bluebox.position.left -= moveSpeed;
+        } else if (e.keyCode == 39) {  //right arrow
+            gc.bluebox.position.left += moveSpeed;
+        }
+        
+        gc.checkLocation(gc.bluebox.position.top, gc.bluebox.position.left);
+    }
+    
+    gc.checkLocation = function(top, left) {
+        if (top < 0) {
+            gc.bluebox.position.top = 0;
+        } else if (top > 590) {
+            gc.bluebox.position.top = 590;
+        }
+        
+        if (left < 0) {
+            gc.bluebox.position.left = 0;
+        } else if (left > 590) {
+            gc.bluebox.position.left = 590;
+        }
+    }
+    
     
 })
